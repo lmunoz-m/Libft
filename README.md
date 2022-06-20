81f3bd276eae03458cec4c33885c8b56456d0c67
Lucia Munoz martinez <lmunoz-m@c1r7s2.42madrid.com>
2022-06-20 19:32:13 +0200

    first steps

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Libft
+
+norminette -R checkForbiddenSourceHeader file.c
+
+For tests
+
+- bash grademe.sh
+- bash grademe.sh -op1
+- bash grademe.sh file.c
+
+
+## Done
+int ft_isalpha(int c);
+
+int ft_isdigit(int c);
+
+int ft_isalnum(int c);
+
+int ft_isascii(int c);
+
+int ft_isprint(int c);
---
+++ libft.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmunoz-m <lmunoz-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/20 18:15:18 by lmunoz-m          #+#    #+#             */
+/*   Updated: 2022/06/20 19:08:10 by lmunoz-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+int ft_strlen(const char *s);
+int ft_toupper(int c);
+
+
+
+
+#endif
